e6b2c77466c2b59415c2fbae4d104a9f4b0fdaca
Konstantin Berkow <konstantinberkow@Konstantins-MacBook-Pro.local>
2017-04-26 22:12:49 +0300

    all those files

---
+++ src/main/java/me/berkow/diffeval/Main.java
@@ -0,0 +1,13 @@
+package me.berkow.diffeval;
+
+import java.util.Arrays;
+
+/**
+ * Created by konstantinberkow on 4/26/17.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.printf("I am maven proj!!!! Args: %s\n", Arrays.toString(args));
+    }
+}
