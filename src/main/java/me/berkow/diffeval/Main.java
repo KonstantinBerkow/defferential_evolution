324c2a74c19bba82657c64f8186ab9b6f56b590d
Konstantin Berkow <konstantinberkow@Konstantins-MacBook-Pro.local>
2017-05-09 10:38:45 +0300

    logging

--- src/main/java/me/berkow/diffeval/Main.java
+++ src/main/java/me/berkow/diffeval/Main.java
@@ -3,8 +3,12 @@ package me.berkow.diffeval;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
-
-import java.util.Arrays;
+import akka.dispatch.OnComplete;
+import akka.event.LoggingAdapter;
+import akka.pattern.Patterns;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import scala.concurrent.Future;
 
 /**
  * Created by konstantinberkow on 4/26/17.
@@ -12,10 +16,28 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.printf("I am maven proj!!!! Args: %s\n", Arrays.toString(args));
+        final Config config = ConfigFactory.load("application.conf");
 
-        ActorSystem system = ActorSystem.create("Hello");
-        ActorRef a = system.actorOf(Props.create(HelloWorld.class), "helloWorld");
-        system.actorOf(Props.create(Terminator.class, a), "terminator");
+        final ActorSystem system = ActorSystem.create("DifferentialEvolution");
+
+        final Props jobActorProps = Props.create(DifferentialEvolutionJobActor.class);
+
+        final ActorRef jobActorRef = system.actorOf(jobActorProps, "JobActor");
+
+        Future<Object> problemSolvage = Patterns.ask(jobActorRef, new DEJob(1), 10000L);
+
+        problemSolvage.onComplete(new OnComplete<Object>() {
+            @Override
+            public void onComplete(Throwable failure, Object success) throws Throwable {
+                LoggingAdapter log = system.log();
+                System.out.println("log: " + log);
+                if (failure != null) {
+                    failure.printStackTrace();
+                } else {
+                    System.out.printf("Success for task #1: %s!\n", success);
+                }
+                system.terminate();
+            }
+        }, system.dispatcher());
     }
 }
