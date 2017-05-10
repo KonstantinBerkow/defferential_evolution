1b12519f18f144784073d243fc4cae27a680e657
Konstantin Berkow <konstantinberkow@Konstantins-MacBook-Pro.local>
2017-05-10 16:48:13 +0300

    multiple launch

---
+++ src/main/java/me/berkow/diffeval/Main.java
@@ -0,0 +1,13 @@
+package me.berkow.diffeval;
+
+/**
+ * Created by konstantinberkow on 5/10/17.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        DEBackendMain.main(new String[]{"2552"});
+        DEBackendMain.main(new String[]{"2553"});
+        DEFrontendMain.main(new String[]{"2551"});
+    }
+}
