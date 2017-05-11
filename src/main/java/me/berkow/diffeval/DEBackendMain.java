ea68ac114db7faa87dc5c80ae627a0ac305aa91b
Konstantin Berkow <konstantinberkow@Konstantins-MacBook-Pro.local>
2017-05-11 12:59:32 +0300

    ips

--- src/main/java/me/berkow/diffeval/DEBackendMain.java
+++ src/main/java/me/berkow/diffeval/DEBackendMain.java
@@ -5,6 +5,8 @@ import akka.actor.Props;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -16,18 +18,28 @@ public class DEBackendMain {
 
     public static void main(String[] args) {
         // Override the configuration of the port and ip when specified as program argument
-        final String hostname = args.length > 0 ? args[0] : null;
+        final String remoteHostname = args.length > 0 ? args[0] : null;
         final String port = args.length > 1 ? args[1] : "0";
 
         Map<String, Object> map = new HashMap<>();
-        if (hostname != null) {
-            map.put("akka.remote.netty.tcp.hostname", hostname);
+        if (remoteHostname != null) {
             map.put("akka.cluster.seed-nodes", Arrays.asList(
-                    "akka.tcp://DifferentialEvolution@" + hostname + ":2552",
-                    "akka.tcp://DifferentialEvolution@" + hostname + ":2553"
+                    "akka.tcp://DifferentialEvolution@" + remoteHostname + ":2552",
+                    "akka.tcp://DifferentialEvolution@" + remoteHostname + ":2553"
             ));
         }
         map.put("akka.remote.netty.tcp.port", port);
+
+        try {
+            InetAddress localHost = InetAddress.getLocalHost();
+
+            String hostAddress = localHost.getHostAddress();
+
+            map.put("akka.remote.netty.tcp.hostname", hostAddress);
+
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
 
         Config config = ConfigFactory.parseMap(map)
                 .withFallback(ConfigFactory.parseString("akka.cluster.roles = [backend]"))
