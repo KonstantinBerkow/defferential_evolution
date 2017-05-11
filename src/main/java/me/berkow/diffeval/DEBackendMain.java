39e0f186d8c5f3a0cad05361d8c1e08a0eed08ef
Konstantin Berkow <konstantinberkow@Konstantins-MacBook-Pro.local>
2017-05-11 11:26:21 +0300

    witj local ip

--- src/main/java/me/berkow/diffeval/DEBackendMain.java
+++ src/main/java/me/berkow/diffeval/DEBackendMain.java
@@ -5,17 +5,33 @@ import akka.actor.Props;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by konstantinberkow on 5/10/17.
  */
 public class DEBackendMain {
 
     public static void main(String[] args) {
-        // Override the configuration of the port when specified as program argument
-        final String port = args.length > 0 ? args[0] : "0";
-        final Config config = ConfigFactory.parseString("akka.remote.netty.tcp.port=" + port).
-                withFallback(ConfigFactory.parseString("akka.cluster.roles = [backend]")).
-                withFallback(ConfigFactory.load());
+        // Override the configuration of the port and ip when specified as program argument
+        final String hostname = args.length > 0 ? args[0] : null;
+        final String port = args.length > 1 ? args[1] : "0";
+
+        Map<String, Object> map = new HashMap<>();
+        if (hostname != null) {
+            map.put("akka.remote.netty.tcp.hostname", hostname);
+            map.put("akka.cluster.seed-nodes", Arrays.asList(
+                    "akka.tcp://DifferentialEvolution@" + hostname + ":2552",
+                    "akka.tcp://DifferentialEvolution@" + hostname + ":2553"
+            ));
+        }
+        map.put("akka.remote.netty.tcp.port", port);
+
+        Config config = ConfigFactory.parseMap(map)
+                .withFallback(ConfigFactory.parseString("akka.cluster.roles = [backend]"))
+                .withFallback(ConfigFactory.load());
 
         ActorSystem system = ActorSystem.create("DifferentialEvolution", config);
 
--- src/main/java/me/berkow/diffeval/Main.java
+++ src/main/java/me/berkow/diffeval/Main.java
@@ -6,8 +6,8 @@ package me.berkow.diffeval;
 public class Main {
 
     public static void main(String[] args) {
-        DEBackendMain.main(new String[]{"2552"});
-        DEBackendMain.main(new String[]{"2553"});
+        DEBackendMain.main(new String[]{"192.168.0.108", "2552"});
+        DEBackendMain.main(new String[]{"192.168.0.108", "2553"});
         DEFrontendMain.main(new String[]{"2551"});
     }
 }
