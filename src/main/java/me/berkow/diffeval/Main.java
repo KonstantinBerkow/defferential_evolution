df2554be96508e944eb8a40984631b43971b1c33
Konstantin Berkow <konstantinberkow@Konstantins-MacBook-Pro.local>
2017-05-11 11:30:23 +0300

    remove hardcode

--- src/main/java/me/berkow/diffeval/Main.java
+++ src/main/java/me/berkow/diffeval/Main.java
@@ -1,13 +1,25 @@
 package me.berkow.diffeval;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 /**
  * Created by konstantinberkow on 5/10/17.
  */
 public class Main {
 
     public static void main(String[] args) {
-        DEBackendMain.main(new String[]{"192.168.0.108", "2552"});
-        DEBackendMain.main(new String[]{"192.168.0.108", "2553"});
+        String hostAddress = "127.0.0.1";
+        try {
+            InetAddress localHost = InetAddress.getLocalHost();
+
+            hostAddress = localHost.getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+        DEBackendMain.main(new String[]{hostAddress, "2552"});
+        DEBackendMain.main(new String[]{hostAddress, "2553"});
         DEFrontendMain.main(new String[]{"2551"});
     }
 }
